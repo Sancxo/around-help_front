122fcd7bd8e68867d09e3e68e24d86a22f41723f
Simon <simon@7lieues.io>
2023-05-18 23:12:44 +0200

    refactor: implements useMenu hook

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,12 +1,14 @@
 import React, { MouseEventHandler, ReactComponentElement, useContext } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { UserContext } from "../shared/context";
+import { useMenu } from "../shared/hooks";
 
-export default function Menu({ logOut, setIsMobileMenuOpen }: {
-  logOut: MouseEventHandler<HTMLAnchorElement>,
-  setIsMobileMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+export default function Menu({ logOut }: {
+  logOut: MouseEventHandler<HTMLAnchorElement>
 }): ReactComponentElement<React.JSXElementConstructor<HTMLDivElement>, Pick<React.ComponentProps<React.JSXElementConstructor<HTMLDivElement>>, keyof HTMLDivElement>> {
   const { pathname } = useLocation();
+
+  const { toggleMenu } = useMenu();
 
   const user_id: number = useContext(UserContext).user.id;
 
@@ -18,19 +20,19 @@ export default function Menu({ logOut, setIsMobileMenuOpen }: {
   const register = "/register";
 
   return (
-    <div className="column flex align-center justify-end ">
+    <div className="column flex align-center justify-end" data-testid="desktop-menu">
       {
         user_id ?
           <div className="flex gap-3 menu">
-            <Link to={profile} title="Your profile" onClick={() => setIsMobileMenuOpen(false)} className={`${pathname === profile && "active"}`}>My profile</Link >
-            <Link to={needs} onClick={() => setIsMobileMenuOpen(false)} className={`${pathname === needs && "active"}`}>Needs</Link>
-            <Link to={messages} onClick={() => setIsMobileMenuOpen(false)} className={`${pathname === messages && "active"}`}>Messages</Link>
-            <Link to="/" onClick={e => { logOut(e); setIsMobileMenuOpen(false); }}>Log out</Link>
+            <Link to={profile} title="Your profile" onClick={() => toggleMenu(false)} className={`${pathname === profile && "active"}`}>My profile</Link >
+            <Link to={needs} onClick={() => toggleMenu(false)} className={`${pathname === needs && "active"}`}>Needs</Link>
+            <Link to={messages} onClick={() => toggleMenu(false)} className={`${pathname === messages && "active"}`}>Messages</Link>
+            <Link to="/" onClick={e => { logOut(e); toggleMenu(false); }}>Log out</Link>
           </div>
           :
           <div className="flex gap-3 menu">
-            <Link to={login} title="Sign in form" onClick={() => setIsMobileMenuOpen(false)} className={`${pathname === login && "active"}`}>Log in</Link>
-            <Link to={register} title="Sign up form" onClick={() => setIsMobileMenuOpen(false)} className={`${pathname === register && "active"}`}>Register</Link>
+            <Link to={login} title="Sign in form" onClick={() => toggleMenu(false)} className={`${pathname === login && "active"}`}>Log in</Link>
+            <Link to={register} title="Sign up form" onClick={() => toggleMenu(false)} className={`${pathname === register && "active"}`}>Register</Link>
           </div>
       }
     </div >
--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,15 +1,16 @@
 import React, { MouseEventHandler, ReactComponentElement } from "react";
 import Menu from "./Menu";
+import { useMenu } from "../shared/hooks";
 
-export default function MobileMenu({ logOut, isMobileMenuOpen, setIsMobileMenuOpen }: {
-  logOut: MouseEventHandler<HTMLAnchorElement>,
-  isMobileMenuOpen: boolean,
-  setIsMobileMenuOpen: React.Dispatch<React.SetStateAction<boolean>>
+export default function MobileMenu({ logOut }: {
+  logOut: MouseEventHandler<HTMLAnchorElement>
 }): ReactComponentElement<React.JSXElementConstructor<HTMLDivElement>, Pick<React.ComponentProps<React.JSXElementConstructor<HTMLDivElement>>, keyof HTMLDivElement>> {
+  const { isMenuOpen, toggleMenu } = useMenu();
+
   return (
-    <div className="flex align-center">
-      <button type="button" onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} className="menu-icon flex align-center mb-0">
-        {isMobileMenuOpen ?
+    <div className="flex align-center" data-testid="mobile-menu">
+      <button type="button" onClick={() => toggleMenu(!isMenuOpen)} className="menu-icon flex align-center mb-0">
+        {isMenuOpen ?
           // Cross icon
           <svg className="w-3 h-3" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd" /></svg>
           :
@@ -17,7 +18,7 @@ export default function MobileMenu({ logOut, isMobileMenuOpen, setIsMobileMenuOp
           <svg className="w-3 h-3" fill="#fff" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" /></svg>
         }
       </button>
-      {isMobileMenuOpen && <Menu logOut={logOut} setIsMobileMenuOpen={setIsMobileMenuOpen} />}
+      {isMenuOpen && <Menu logOut={logOut} />}
     </div >
   )
 }
