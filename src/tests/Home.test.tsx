ddcd44c9ba71e261166e80045defd96985bf89e7
Simon <simon@7lieues.io>
2023-05-19 17:07:02 +0200

    tests: adds a new test to Home page

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -1,18 +1,37 @@
 import { render, screen } from "@testing-library/react"
 import Home from "../pages/Home"
-import { MemoryRouter } from "react-router-dom"
-import AppProvider from "../shared/context"
+import { registeredUserTest, serverEvents, wrapRouteAndRender } from "./test-helpers"
+import { UserContext } from "../shared/context";
+import { MemoryRouter } from "react-router-dom";
+import { setupServer } from "msw/lib/node";
+import { rest } from "msw";
+
+const server = setupServer(
+  rest.get(
+    `${process.env.REACT_APP_BACKEND_URL}/unfulfilled_needs`,
+    (req, res, ctx) => {
+      return res(ctx.json(404));
+    }
+  )
+);
+serverEvents(server);
 
 describe("Home", () => {
   it('should return a "Login" button', () => {
-    render(
-      <MemoryRouter>
-        <AppProvider>
-          <Home />
-        </AppProvider>
-      </MemoryRouter>
-    )
+    wrapRouteAndRender(<Home />);
 
     expect(screen.getByText('Log in')).toBeTruthy();
+  });
+
+  it('should return a button link to Needs page', async () => {
+    render(
+      <MemoryRouter>
+        <UserContext.Provider value={{ user: registeredUserTest, setUser: (user) => user }}>
+          <Home />
+        </UserContext.Provider>
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Answer to a Need')).toBeTruthy();
   })
 })
